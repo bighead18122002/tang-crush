0afaa3dd7a614e470c5823b05ba8e1e273186b6b
bighead18122002 <86771455+bighead18122002@users.noreply.github.com>
2021-07-01 15:49:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Hé lô bạn Quỳnh!',
+    introDesc: ` là lá laa`,
+    btnIntro: 'hehe',
+    title: 'Bạn có 2 sự lựa chọn',
+    desc: 'Nhất Phong hay Park Ji Min ?',
+    btnYes: 'Đương nhiên là Nhất Phong ròii',
+    btnNo: 'Không, em trai à, oppa chị là số 1 :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Quỳnh lại chọn tuii <3',
+    btnReply: 'Gửi cho Phong đáng iu <3',
+    reply: 'ngàn trái timm <33333333',
+    mess: 'tui biết màa hehe =))))',
+    messDesc: 'Tối nay 7h tui qua chở bạn đi nhậu, ok chưa ?.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://www.facebook.com/nhatphong1812' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
